6cfa4b6309710869964ae7c4a1073ba82993be8e
ohyeahcity <owlpeipei@gmail.com>
2016-08-17 14:36:25 +0800

    Add files via upload

---
+++ js/J_S_Multi_language_LT_v0.9.js
@@ -0,0 +1,252 @@
+//多語系動態網頁介面配搭CSS切換程式碼
+//Coding by Jerry Shih @ Quanta Computer Inc. - 2016/07/05 ver 0.9 LT
+//使用時必須在HTML HEAD標籤掛入本 J_S_Multi_language_v*.*.js檔案
+//主要功能1 : Multi-lang Title 根據 on_click 事件切換網頁標題
+//主要功能2 : 賦予Body 語系標籤，例如 TW,ENG,JP。以便讓不同的CSS樣式來切換背景圖片、甚至文字
+//主要功能3 : 使用DOM方法，更換下拉顯示介面, 須掛入JSON資源文件檔
+//主要功能4 : 使用DOM方法，依照語系更改文件內的文字
+
+// 其他功能a (HTML) : 自動偵測瀏覽器語言，更改語言預設值。
+
+
+
+
+//=====程式開始======
+
+
+
+
+
+
+function chg_lang(lang_index){
+
+
+			//更改標題文字
+      changeWebTitle(lang_index);
+
+      //更改BODY 語系標籤
+      changeCSSTag_Multi_Lang(lang_index);
+      //更改Navbar文件
+      changeNavBarUIWording(lang_index);
+      //根據語系更改圖片
+      changeImageByLang(lang_index);
+
+      //讀外部JSON檔案
+      var xmlhttp = new XMLHttpRequest();
+      var url = "mlang.txt";
+
+      xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          var myArr = JSON.parse(xmlhttp.responseText);
+          console.log(myArr);
+          changeAllNavBarUIWording(myArr,lang_index);
+          }
+          console.log("wording changed! done.");
+      };
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+
+
+
+
+
+		}
+
+//主要功能1 : Multi-lang Title 根據 on_click 事件切換網頁標題
+function changeWebTitle(lang_index){
+      //變更多語系網頁Title
+      document.getElementsByTagName("title")[0].innerHTML = Multi_Lang_Title[lang_index];
+      return;
+
+
+}
+
+//主要功能2 : 賦予Body 語系標籤，例如 TW,ENG,JP。以便讓不同的CSS樣式來切換背景圖片
+function changeCSSTag_Multi_Lang(lang_index){
+    // jQuery 語法:
+    //  $("body").removeClass("tw cn en").addClass($(this).data("資料名"));
+
+    //純DOM語法
+    document.getElementsByTagName("body")[0].className = "";
+    document.getElementsByTagName("body")[0].className = Multi_Lang_Tag[lang_index];//Multi_Lang_Tag[lang_index];
+    return;
+}
+
+//主要功能3 : 使用DOM方法，更換下拉&Navbar顯示介面
+function changeNavBarUIWording(lang_index){
+
+  //更改Drondown UI 語系顯示
+  //document.getElementById('lang_btn_1').innerHTML = Multi_Lang_Wording[lang_index];
+
+  document.getElementById('change_dropdown_title').innerHTML =  Multi_Lang_Wording[lang_index]+'<b class="caret"></b>';
+
+  return;
+}
+
+//控制圖片中的語系
+function changeImageByLang(lang_index){
+
+
+  switch (lang_index) {
+
+    case 0:
+          document.getElementById('slideshowImage1').src = "images/slider_example_1.jpg" ;
+          document.getElementById('slideshowImage2').src = "images/slider_example_2.jpg" ;
+          document.getElementById('download_tl_img').src = "images/download_title.jpg" ;
+          document.getElementById('lang_pic_cause').src = "images/come_img01.jpg" ;
+          document.getElementById('lang_comeright').src = "images/PM_info.png" ;
+          document.getElementById('lang_blue_img').src = "images/bluetooth_bg.jpg" ;
+          document.getElementById('lang_blue_img_min').src = "images/bluetooth_bg_min.jpg" ;
+          console.log("image is cht");
+
+      break;
+    case 1:
+        document.getElementById('slideshowImage1').src = "images/slider_example_1_eng.jpg" ;
+        document.getElementById('slideshowImage2').src = "images/slider_example_2_eng.jpg" ;
+        document.getElementById('download_tl_img').src = "images/download_title_eng.jpg" ;
+        document.getElementById('lang_pic_cause').src = "images/come_img01_eng.jpg" ;
+        document.getElementById('lang_comeright').src = "images/PM_info_eng.png" ;
+        document.getElementById('lang_blue_img').src = "images/bluetooth_bg_eng.jpg" ;
+        document.getElementById('lang_blue_img_min').src = "images/bluetooth_bg_min_eng.jpg" ;
+
+        console.log("english image now");
+      break;
+    default:
+      document.getElementById('slideshowImage1').src = "images/slider_example_1.jpg" ;
+      document.getElementById('slideshowImage2').src = "images/slider_example_2.jpg" ;
+      document.getElementById('download_tl_img').src = "images/download_title.jpg" ;
+      document.getElementById('lang_pic_cause').src = "images/come_img01.jpg" ;
+      document.getElementById('lang_comeright').src = "images/PM_info.png" ;
+
+      console.log("image is default cht");
+      break;
+
+  }
+  return;
+
+}
+
+
+
+
+
+
+
+
+
+
+// 根據語系更改所有文字
+function changeAllNavBarUIWording(arr,lang_index){
+/*
+  document.getElementById('nav_link1_wording').innerHTML = arr[lang_index].NavLink1;
+  document.getElementById('nav_link2_wording').innerHTML = arr[lang_index].NavLink2;
+  document.getElementById('nav_link3_wording').innerHTML = arr[lang_index].NavLink3;
+  document.getElementById('nav_link4_wording').innerHTML = arr[lang_index].NavLink4;
+  document.getElementById('nav_link5_wording').innerHTML = arr[lang_index].NavLink5;*/
+
+  document.getElementById('lang_navproduct').innerHTML = arr[lang_index].lang_navproduct;
+  document.getElementById('lang_navscenario').innerHTML = arr[lang_index].lang_navscenario;
+  document.getElementById('lang_navspec').innerHTML = arr[lang_index].lang_navspec;
+  document.getElementById('lang_navload').innerHTML = arr[lang_index].lang_navload;
+  document.getElementById('lang_navfaq').innerHTML = arr[lang_index].lang_navfaq;
+  document.getElementById('lang_navch').innerHTML = arr[lang_index].lang_navch;
+  document.getElementById('lang_naven').innerHTML = arr[lang_index].lang_naven;
+
+  document.getElementById('lang_comeleft_cont01').innerHTML = arr[lang_index].lang_comeleft_cont01;
+  document.getElementById('lang_comeleft_cont02').innerHTML = arr[lang_index].lang_comeleft_cont02;
+
+  document.getElementById('lang_deep_title').innerHTML = arr[lang_index].lang_deep_title;
+  document.getElementById('lang_deep_cont01').innerHTML = arr[lang_index].lang_deep_cont01;
+  document.getElementById('lang_deep_cont02').innerHTML = arr[lang_index].lang_deep_cont02;
+  document.getElementById('lang_deep_cont03').innerHTML = arr[lang_index].lang_deep_cont03;
+  document.getElementById('lang_feature_title').innerHTML = arr[lang_index].lang_feature_title;
+  document.getElementById('lang_feature_cont01').innerHTML = arr[lang_index].lang_feature_cont01;
+  document.getElementById('lang_feature_cont02').innerHTML = arr[lang_index].lang_feature_cont02;
+  document.getElementById('lang_feature_cont03').innerHTML = arr[lang_index].lang_feature_cont03;
+  document.getElementById('lang_vent_title').innerHTML = arr[lang_index].lang_vent_title;
+  document.getElementById('lang_light_title').innerHTML = arr[lang_index].lang_light_title;
+  document.getElementById('lang_download_title').innerHTML = arr[lang_index].lang_download_title;
+  document.getElementById('lang_bl_title').innerHTML = arr[lang_index].lang_bl_title;
+  document.getElementById('lang_bl_text').innerHTML = arr[lang_index].lang_bl_text;
+  document.getElementById('lang_download_text01').innerHTML = arr[lang_index].lang_download_text01;
+  document.getElementById('lang_iphone_title').innerHTML = arr[lang_index].lang_iphone_title;
+  document.getElementById('lang_iphone_text').innerHTML = arr[lang_index].lang_iphone_text;
+  document.getElementById('lang_download_text02').innerHTML = arr[lang_index].lang_download_text02;
+  document.getElementById('lang_win_title').innerHTML = arr[lang_index].lang_win_title;
+  document.getElementById('lang_win_text01').innerHTML = arr[lang_index].lang_win_text01;
+  document.getElementById('lang_download_text03').innerHTML = arr[lang_index].lang_download_text03;
+  document.getElementById('lang_spec_title').innerHTML = arr[lang_index].lang_spec_title;
+  document.getElementById('lang_spec_title01').innerHTML = arr[lang_index].lang_spec_title01;
+  document.getElementById('lang_spec_text01').innerHTML = arr[lang_index].lang_spec_text01;
+  document.getElementById('lang_spec_title02').innerHTML = arr[lang_index].lang_spec_title02;
+  document.getElementById('lang_spec_text02').innerHTML = arr[lang_index].lang_spec_text02;
+  document.getElementById('lang_spec_title03a').innerHTML = arr[lang_index].lang_spec_title03a;
+  document.getElementById('lang_spec_text03').innerHTML = arr[lang_index].lang_spec_text03;
+  document.getElementById('lang_spec_text03_1').innerHTML = arr[lang_index].lang_spec_text03_1;
+  document.getElementById('lang_spec_title04').innerHTML = arr[lang_index].lang_spec_title04;
+  document.getElementById('lang_spec_text04').innerHTML = arr[lang_index].lang_spec_text04;
+  document.getElementById('lang_spec_title05').innerHTML = arr[lang_index].lang_spec_title05;
+  document.getElementById('lang_spec_text05').innerHTML = arr[lang_index].lang_spec_text05;
+  document.getElementById('lang_spec_title06').innerHTML = arr[lang_index].lang_spec_title06;
+  document.getElementById('lang_spec_text06').innerHTML = arr[lang_index].lang_spec_text06;
+
+
+
+  return;
+}
+
+
+
+
+
+function detectUserLang(){
+
+    var tempLang = window.navigator.userLanguage || window.navigator.language ;
+    var currentBrowserLang = tempLang.toLowerCase();
+    console.log(currentBrowserLang);
+
+    switch (currentBrowserLang) {
+      case "zh-tw":
+            chg_lang(0);
+        break;
+      case "zh-cn":
+            chg_lang(0);
+        break;
+      case "zh-hk":
+            chg_lang(0);
+        break;
+      case "ja":
+            chg_lang(2);
+        break;
+
+      default:
+            chg_lang(1);
+        break;
+    }
+}
+
+
+
+
+//=====文字儲存區======
+//多語系body標籤
+Multi_Lang_Tag = new Array();
+Multi_Lang_Tag[0]="tw";
+Multi_Lang_Tag[1]="eng";
+Multi_Lang_Tag[2]="jp";
+
+//Multi_Lang_Wording 供介面顯示
+Multi_Lang_Wording = new Array();
+Multi_Lang_Wording[0]="TW - 繁體中文";
+Multi_Lang_Wording[1]="EN - English";
+Multi_Lang_Wording[2]="JP - 日本語";
+
+
+//多語系網頁Title招呼語 (顯示在瀏覽TAB上)
+Multi_Lang_Title= new Array();
+Multi_Lang_Title[0]="歡迎來到EQL - PM2.5網站";
+Multi_Lang_Title[1]="Welcome to EQL - PM2.5";
+Multi_Lang_Title[2]="EQLへようこそ - PM2.5";
+
+
+//=====文字儲存區 END======
